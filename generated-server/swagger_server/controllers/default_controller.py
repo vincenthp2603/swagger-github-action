2dd00c457791dd3d55974e57c94a8c46aad70420
vincenthp2603 <runner@fv-az204-278.d0ggoinxmwcejgojbuqly40l0e.cx.internal.cloudapp.net>
2022-11-18 13:49:13 +0000

    211bef3 - Fri Nov 18 13:49:13 UTC 2022: amend

---
+++ generated-server/swagger_server/controllers/default_controller.py
@@ -0,0 +1,142 @@
+import connexion
+import six
+
+from swagger_server import util
+
+
+def alarms_controller_create():  # noqa: E501
+    """alarms_controller_create
+
+     # noqa: E501
+
+
+    :rtype: None
+    """
+    return 'do some magic!'
+
+
+def alarms_controller_delete(id):  # noqa: E501
+    """alarms_controller_delete
+
+     # noqa: E501
+
+    :param id: 
+    :type id: dict | bytes
+
+    :rtype: None
+    """
+    if connexion.request.is_json:
+        id = .from_dict(connexion.request.get_json())  # noqa: E501
+    return 'do some magic!'
+
+
+def alarms_controller_find(ProductInstanceUri=None):  # noqa: E501
+    """alarms_controller_find
+
+     # noqa: E501
+
+    :param ProductInstanceUri: 
+    :type ProductInstanceUri: dict | bytes
+
+    :rtype: None
+    """
+    if connexion.request.is_json:
+        ProductInstanceUri = .from_dict(connexion.request.get_json())  # noqa: E501
+    return 'do some magic!'
+
+
+def alarms_controller_update(id):  # noqa: E501
+    """alarms_controller_update
+
+     # noqa: E501
+
+    :param id: 
+    :type id: dict | bytes
+
+    :rtype: None
+    """
+    if connexion.request.is_json:
+        id = .from_dict(connexion.request.get_json())  # noqa: E501
+    return 'do some magic!'
+
+
+def app_controller_get_hello():  # noqa: E501
+    """app_controller_get_hello
+
+     # noqa: E501
+
+
+    :rtype: None
+    """
+    return 'do some magic!'
+
+
+def assets_controller_create():  # noqa: E501
+    """assets_controller_create
+
+     # noqa: E501
+
+
+    :rtype: None
+    """
+    return 'do some magic!'
+
+
+def assets_controller_delete(ProductInstanceUri):  # noqa: E501
+    """assets_controller_delete
+
+     # noqa: E501
+
+    :param ProductInstanceUri: 
+    :type ProductInstanceUri: dict | bytes
+
+    :rtype: None
+    """
+    if connexion.request.is_json:
+        ProductInstanceUri = .from_dict(connexion.request.get_json())  # noqa: E501
+    return 'do some magic!'
+
+
+def assets_controller_find(ProductInstanceUri=None, PublisherInstanceUri=None):  # noqa: E501
+    """assets_controller_find
+
+     # noqa: E501
+
+    :param ProductInstanceUri: 
+    :type ProductInstanceUri: dict | bytes
+    :param PublisherInstanceUri: 
+    :type PublisherInstanceUri: dict | bytes
+
+    :rtype: None
+    """
+    if connexion.request.is_json:
+        ProductInstanceUri = .from_dict(connexion.request.get_json())  # noqa: E501
+    if connexion.request.is_json:
+        PublisherInstanceUri = .from_dict(connexion.request.get_json())  # noqa: E501
+    return 'do some magic!'
+
+
+def events_controller_create():  # noqa: E501
+    """events_controller_create
+
+     # noqa: E501
+
+
+    :rtype: None
+    """
+    return 'do some magic!'
+
+
+def events_controller_find(ProductInstanceUri=None):  # noqa: E501
+    """events_controller_find
+
+     # noqa: E501
+
+    :param ProductInstanceUri: 
+    :type ProductInstanceUri: dict | bytes
+
+    :rtype: None
+    """
+    if connexion.request.is_json:
+        ProductInstanceUri = .from_dict(connexion.request.get_json())  # noqa: E501
+    return 'do some magic!'
