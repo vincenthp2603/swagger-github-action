0d9aa9aa4007552f5023d0d280e9bd24782b1050
vincenthp2603 <runner@fv-az296-490.ie1vkwtfhimuhe55j3he1m2lab.gx.internal.cloudapp.net>
2022-12-05 12:40:14 +0000

    25e5331 - Mon Dec  5 12:40:14 UTC 2022:

--- server/swagger_server/controllers/default_controller.py
+++ server/swagger_server/controllers/default_controller.py
@@ -7,7 +7,6 @@ from swagger_server.models.post_event_dto import PostEventDto  # noqa: E501
 from swagger_server.models.put_alarm_dto import PutAlarmDto  # noqa: E501
 from swagger_server import util
 
-from swagger_server.models.mongodb.schemas.asset import Asset
 
 def alarms_controller_create(body):  # noqa: E501
     """alarms_controller_create
@@ -75,7 +74,7 @@ def app_controller_get_hello():  # noqa: E501
 
     :rtype: None
     """
-    return 'Hello'
+    return 'do some magic!'
 
 
 def assets_controller_create(body):  # noqa: E501
@@ -89,11 +88,9 @@ def assets_controller_create(body):  # noqa: E501
     :rtype: None
     """
     if connexion.request.is_json:
-        #body = PostAssetDto.from_dict(connexion.request.get_json())  # noqa: E501
-        post_asset_dto = connexion.request.get_json()
-        asset = Asset(**post_asset_dto)
-        asset.save()
-    return str(asset.id)
+        body = PostAssetDto.from_dict(connexion.request.get_json())  # noqa: E501
+    return 'do some magic!'
+
 
 def assets_controller_delete(ProductInstanceUri):  # noqa: E501
     """assets_controller_delete
@@ -105,9 +102,8 @@ def assets_controller_delete(ProductInstanceUri):  # noqa: E501
 
     :rtype: None
     """
-    asset = Asset.objects(ProductInstanceUri=ProductInstanceUri).first()
-    asset.delete()
-    return str(asset.id)
+    return 'do some magic!'
+
 
 def assets_controller_find(ProductInstanceUri=None, PublisherInstanceUri=None):  # noqa: E501
     """assets_controller_find
@@ -121,13 +117,7 @@ def assets_controller_find(ProductInstanceUri=None, PublisherInstanceUri=None): 
 
     :rtype: None
     """
-    if ProductInstanceUri is not None:
-        assets = Asset.objects(ProductInstanceUri=ProductInstanceUri)
-    elif PublisherInstanceUri is not None:
-        assets = Asset.objects(PublisherInstanceUri=PublisherInstanceUri)
-    else:
-        assets = []
-    return assets.to_json()
+    return 'do some magic!'
 
 
 def events_controller_create(body):  # noqa: E501
